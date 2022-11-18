71d906e4140547fd94418bfd91de86e0b393b2e3
jitendrafidel <jitendra.muradnar.fidel@gmail.com>
2022-11-18 20:26:42 +0530

    fix : made sure create bucket and ecr repo

--- pipelines/lib/eth-socket-collector-pipeline-stack.ts
+++ pipelines/lib/eth-socket-collector-pipeline-stack.ts
@@ -1,7 +1,16 @@
-import * as ecr from 'aws-cdk-lib/aws-ecr';
-import {App, CfnOutput, Duration, Fn, RemovalPolicy, Stack, StackProps} from 'aws-cdk-lib';
-import { GithubEcrPipeline } from './constructs/github-ecr-pipeline/github-ecr-construct';
-import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as ecr from "aws-cdk-lib/aws-ecr";
+import {
+  App,
+  CfnOutput,
+  Duration,
+  Fn,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+} from "aws-cdk-lib";
+import { GithubEcrPipeline } from "./constructs/github-ecr-pipeline/github-ecr-construct";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 
 export interface PipelineStackProps extends StackProps {
   branchName: string;
@@ -12,29 +21,36 @@ export class ETHSocketClientPipelineStack extends Stack {
 
     const { branchName } = props!;
 
-    const repository = new ecr.Repository(this, 'eth-socket-collector', {
-      repositoryName: 'eth-socket-collector',
-      removalPolicy: RemovalPolicy.DESTROY
+    const repository = new ecr.Repository(this, "eth-socket-collector", {
+      repositoryName: "eth-socket-collector",
+      removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    const bucketName = Fn.importValue('artifact-bucket');
+    // const bucketName = Fn.importValue("artifact-bucket");
 
-    const bucket = s3.Bucket.fromBucketName(this, 's3-bucket', bucketName);
+    const bucket = new s3.Bucket(this, "s3-bucket", {
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
 
-    repository.addLifecycleRule({ tagPrefixList: ['prod'], maxImageCount: 60 });
+    repository.addLifecycleRule({ tagPrefixList: ["prod"], maxImageCount: 60 });
     repository.addLifecycleRule({ maxImageAge: Duration.days(30) });
 
-    const pipeline = new GithubEcrPipeline(this, 'eth-wss-client-pipeline', {
+    const pipeline = new GithubEcrPipeline(this, "eth-wss-client-pipeline", {
       ecrRepository: repository,
-      githubRepo: 'websocket-clients',
+      githubRepo: "websocket-clients",
       branchName: branchName,
-      directoryName: 'services/eth-socket-client',
-      artifactBucket: bucket
+      directoryName: "services/eth-socket-client",
+      artifactBucket: bucket,
     });
 
-    new CfnOutput(this, 'eth-socket-collector-repo', {
-      value: repository.repositoryName,
-      exportName: 'eth-socket-collector-repo',
+    new ssm.StringParameter(this, "ECRRepoParameter", {
+      parameterName: `/websocket-clients/evm-socket-collector`,
+      stringValue: repository.repositoryName,
+    });
+
+    new ssm.StringParameter(this, "ArtifactBucket", {
+      parameterName: `/websocket-clients/artifact-bucket`,
+      stringValue: bucket.bucketName,
     });
   }
 }
