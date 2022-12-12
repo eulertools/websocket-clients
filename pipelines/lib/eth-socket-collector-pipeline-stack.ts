97877ecdb616a601fb1f2e045dc529cd575f5fe0
tebayoso <tebayoso@gmail.com>
2022-12-12 12:37:16 +0100

    Foxed names

--- pipelines/lib/eth-socket-collector-pipeline-stack.ts
+++ pipelines/lib/eth-socket-collector-pipeline-stack.ts
@@ -1,5 +1,5 @@
 import * as ecr from 'aws-cdk-lib/aws-ecr';
-import {App, CfnOutput, Duration, Fn, RemovalPolicy, Stack, StackProps} from 'aws-cdk-lib';
+import { App, Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { GithubEcrPipeline } from './constructs/github-ecr-pipeline/github-ecr-construct';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as ssm from "aws-cdk-lib/aws-ssm";
@@ -7,17 +7,18 @@ import * as ssm from "aws-cdk-lib/aws-ssm";
 export interface PipelineStackProps extends StackProps {
   branchName: string;
 }
+
 export class ETHSocketClientPipelineStack extends Stack {
   constructor(scope: App, id: string, props?: PipelineStackProps) {
     super(scope, id, props);
 
-    const { branchName } = props!;
+    const {branchName} = props!;
 
     const repository = new ecr.Repository(this, 'Repository', {
       removalPolicy: RemovalPolicy.DESTROY
     });
-    repository.addLifecycleRule({ tagPrefixList: ['prod'], maxImageCount: 60 });
-    repository.addLifecycleRule({ maxImageAge: Duration.days(30) });
+    repository.addLifecycleRule({tagPrefixList: ['prod'], maxImageCount: 60});
+    repository.addLifecycleRule({maxImageAge: Duration.days(30)});
 
     // const bucketName = Fn.importValue("artifact-bucket");
 
@@ -29,7 +30,7 @@ export class ETHSocketClientPipelineStack extends Stack {
       ecrRepository: repository,
       githubRepo: "websocket-clients",
       branchName: branchName,
-      directoryName: 'services/binance-client',
+      directoryName: 'services/evm-socket-client',
     });
 
     new ssm.StringParameter(this, 'RepositorySSM', {
